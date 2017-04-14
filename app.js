28f549476bf371d0e42d351d98355b86e77f7c3f
cale312 <zikisacalesogaxa@gmail.com>
2017-04-14 16:28:11 +0200

    Deleted the handlebars compile code. Added a code to create new element and append to the dom. Added an event listener code to target a click on the appended li tags to draw line through it task is clicked

--- app.js
+++ app.js
@@ -1,41 +1,25 @@
-var taskInput = document.querySelector('.task');
-var taskList = [];
-var checkList = document.querySelector('.taskList');
+var taskInput = document.querySelector('.taskInput');
 var add = document.querySelector('.add-btn');
-
-var checkBox = document.querySelector('.checkBox').innerHTML;
-var template =  Handlebars.compile(checkBox);
-
-checkList.innerHTML = template({list: taskList});
-
+var list = document.getElementsByTagName('li');
 add.addEventListener('click', function () {
     'use strict';
-    var newTaskList = {task: taskInput.value};
     if (taskInput.value !== "") {
-        taskList.push(newTaskList);
-        checkList.innerHTML = template({list: taskList});
+        var input = document.createTextNode(taskInput.value);
+        var newTask = document.createElement('li');
+        newTask.appendChild(input);
+        document.querySelector('.Tasklist').appendChild(newTask);
     }
     taskInput.value = "";
 });
-
-checkList.addEventListener('click', function (evt) {
-    'use strict';
-    var taskToDelete = evt.target.value;
-    var index = taskList.indexOf(taskToDelete);
-    var newTaskList = [];
-    for (var i = 0; i < taskList.length; i++) {
-        var listTask = taskList[i];
-        if (listTask.task != taskToDelete) {
-            newTaskList.push(listTask)
+document.querySelector('.Tasklist').addEventListener('click', function (evt) {
+    var taskFinished = evt.target;
+    for (var i = 0; i < list.length; i++) {
+        if (list[i] === taskFinished) {
+            list[i].style.textDecoration = 'line-through';
+            list[i].style.color = 'red';
         }
     }
-    taskList.splice(index, taskToDelete);
-    localStorage.setItem('taskList', JSON.stringify(taskList))
-    taskList = newTaskList;
-    if (taskToDelete != "on") {
-        checkList.innerHTML = template({list: taskList});
-    }
 });
-document.querySelector('.date').addEventListener('click', function () {
-    document.querySelector('.date').value = "";
+document.querySelector('.dateInput').addEventListener('click', function () {
+    document.querySelector('.dateInput').value = "";
 });
